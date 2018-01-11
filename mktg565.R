3926944b599220a875f6b2dc0320b88f0d5f7836
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-01-10 18:01:24 -0800

    begin exploratory analysis
    trying to make plots, while splitting them by CHURN as a facet

--- mktg565.R
+++ mktg565.R
@@ -4,6 +4,11 @@
 
 
 #  Load the .sav (spss) data file into R
+install.packages('reshape2')
+install.packages('plotly')
+install.packages('tabplot')
+install.packages('dplyr')
+install.packages('Amelia')
 install.packages('haven')
 library(haven)
 # code for iMac
@@ -17,7 +22,8 @@ summary(Cell_Data)
 #  view the structure of the data
 str(Cell_Data)
 #  identify columns that are not useful
-Worthless <- c("INCMISS","SETPRCM","CHURNDEP")
+Worthless <- c("INCMISS","SETPRCM","CHURNDEP","CSA")
+DropColIndex <- match(Worthless,colnames(Cell_Data))
 #  identify column that we want to predict
 Y_dependent <- "CHURN"
 #  identify profitability metrics that we want to maximize
@@ -37,76 +43,87 @@ head(Cell_Data)[,61:70]
 head(Cell_Data)[,71:ColSize]
 
 # Find missing data "NA" by columns
-MissingReport <- data.frame(Columns,x)
-colnames(MissingReport) <- c("Columns","NA_Count")
-
-#  clean the data (remove any rows with NA column values)
-Cell_naOMIT <- na.omit(Cell_Data)
-
-# we now have 38941 rows rather than 71047 rows...we lost 32106 rows
-71047 - 38941
-# double check to see if this is the correct amount of rows to delete
-# .... this code takes too long to run.
-#  But as I stopped it early on, i noticed that the column 78 CHURNDEP
-#  had almost all of the NA's
-#     This CHURNDEP was the variable that is to separate our test set
-#     with the validation set
-'''
-BadRows <- rep(0,71047)
-ColBadCount <- data.frame(colnames(Cell_Data),rep(0,78))
-colnames(ColBadCount) <- c("colNames","countNA")
-for (i in 1:71048){
-   #print(i)
-   for (j in 1:78){
-      x <- Cell_Data[i,j]
-      if (is.na(x)){
-         BadRows[i] <- 1
-         ColBadCount$countNA[j] = ColBadCount$countNA[j] +1
-      }
-   }
-}
-'''
-# double check validation vs. calibration sample
-Cell_Data$CALIBRAT[1:10]
-Cell_Data$CHURNDEP[1:10]
-table(Cell_Data[,c(77,78)],exclude=NULL)
-
-# examine the CHURNDEP and CHURN columns
-summary(Cell_Data$CHURNDEP)
-hist(Cell_Data$CHURNDEP)
-table(Cell_Data$CHURNDEP)
-table(Cell_Data$CHURN)
-
-sum(is.na(Cell_Data[,1]))
-sum(is.na(Cell_Data$CHURNDEP))
-
 x <- rep(0,ColSize)
 for (i in 1:ColSize){
   x[i] <- sum(is.na(Cell_Data[,i]))
   print(x)
 }
+MissingReport <- data.frame(Columns,x)
+colnames(MissingReport) <- c("Columns","NA_Count")
+MissingReport
+
+# Separate the Training Set and Validation Set
+# double check validation vs. calibration sample
+table(Cell_Data[,c(77,78)],exclude=NULL)
+table(Cell_Data[,c(77,22)],exclude=NULL)
+
+# Make Training Set
+Training <- Cell_Data[Cell_Data$CALIBRAT == 0,-DropColIndex]
+
+# Make Test Set
+Test <- Cell_Data[Cell_Data$CALIBRAT == 1,-DropColIndex]
+# Double Check the results
+summary(Test)
+summary(Training)
+str(Test)
+str(Training)
+
+
+#  clean the data (remove any rows with NA column values)
+#Cell_naOMIT <- na.omit(Cell_Data)
+library(Amelia)
+missmap(Training, main = "Missing values vs observed")
+
+ColSizeTrain <- length(colnames(Training))
+x <- rep(0,ColSizeTrain)
+for (i in 1:ColSizeTrain){
+  x[i] <- sum(is.na(Training[,i]))
+  print(x)
+}
+MissingReportTrain <- data.frame(colnames(Training),x)
+colnames(MissingReportTrain) <- c("Columns","NA_Count")
+MissingReportTrain
+Clean_Train <- na.omit(Training)
+#count of lost rows after removing NA's
+31047-30368
 
 
 
+# Exploratory data analysis on the Training Set
+pairs(Clean_Train[,1:3])
+cor(Clean_Train[,1:ColSizeTrain])
+cor(Clean_Train[,1:10])
 
-
-
-
-
-'''
-hist(Cell2Cell_SPSS_Data$REVENUE)
-hist(Cell2Cell_SPSS_Data$OVERAGE)
-hist(Cell2Cell_SPSS_Data$CHURN)
-plot(Cell2Cell_SPSS_Data$OVERAGE, Cell2Cell_SPSS_Data$REVENUE)
-
-
-
-
-
-
+# randomly sample 1000 rows for scatter plot matrix
+# for quicker plotting
+library(dplyr)
+library(tabplot)
 library(ggplot2)
-?qplot()
-qplot(Cell2Cell_SPSS_Data, Cell2Cell_SPSS_Data)
-qplot(data=diamonds,carat,price,colour=clarity,facets=.~clarity)
-qplot(data=Cell2Cell_SPSS_Data, DROPVCE, REVENUE, color = CHURN, alpha=.3)
-'''
+Train_samp1000 <- sample_n(Training,1000)
+pairs(Train_samp1000[,1:74])
+tableplot(Train_samp1000)
+
+par(mfrow=c(2,1))
+boxplot(Train_samp1000$OVERAGE,Train_samp1000$CHURN)
+plot(Train_samp1000$OVERAGE,Train_samp1000$CHURN)
+hist(Train_samp1000$REVENUE)
+
+hp <- ggplot(tips, aes(x=total_bill)) + geom_histogram(binwidth=2,colour="white")
+
+hp <- ggplot(Training, aes(x=OVERAGE)) + geom_histogram(binwidth=10,colour="black")
+hp + facet_grid(CHURN ~ .)
+hist(Training$OVERAGE[Train_samp1000$CHURN==0])
+hist(Training$OVERAGE[Train_samp1000$CHURN==1])
+
+# try putting box plots or histograms on the SAME plot, so they sahre the SAME axis
+
+
+
+
+
+
+
+
+
+
+
